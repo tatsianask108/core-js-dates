2af2e2db1b708faff768b96f33577bd93fee967b
tatsianask108 <tt.laurie@gmail.com>
2024-03-20 22:01:45 +0300

    feat: pass 12 / 14

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -245,8 +245,9 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  return Math.floor((month + 3) / 3);
 }
 
 /**
@@ -283,8 +284,9 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return year % 4 === 0 || (year % 100 === 0 && year % 400 === 0);
 }
 
 module.exports = {
